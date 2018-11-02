d72151980c49a331b6df99c6efb4bd4641aee96d
praemaii <59160596@go.buu.ac.th>
2018-11-02 10:21:03 +0700

    First commit

---
+++ src/NewClass.java
@@ -0,0 +1,32 @@
+import com.mongodb.*;
+import com.mongodb.MongoClient;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class NewClass {
+   
+ 
+    public static void main(String[] args) {
+         DBCollection table;
+    DB db;
+    try { 
+        
+    
+        
+//คา สงั่ ในการเชื่อมต่อขอ้มูล
+System.out.println("Connect successfully");
+//การแสดงผล Connect successfully
+} catch (Exception e) {
+}
+}
+}
+    
+
---
+++ src/com/java/myapp/cb.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.myapp;
+
+/**
+ *
+ * @author Acer
+ */
+class cb {
+
+    static Object getItemCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getSelectedItem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void removeAllItems() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void addItem(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void removeItem(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
